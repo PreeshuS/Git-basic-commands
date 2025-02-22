6bcd1b61fc16500a0478ead62feec41383254441
preeshu <preeshu.srivastava84@gmail.com>
2025-02-22 20:35:45 +0000

    resolving merge conflict

--- git_tutorial.py
+++ git_tutorial.py
@@ -1,10 +1,8 @@
 def add_number(a,b,c):
     return a + b + c
 
-<<<<<<< HEAD
 def difference(a,b):
-    return a- b
-=======
+    return a - b
+
 def add(a,b,c):
     return sum([a, b, c])
->>>>>>> master
